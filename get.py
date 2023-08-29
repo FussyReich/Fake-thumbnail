d445a3a437e1cb378abbf3fc1abf6c7096f60590
FussyReich <okano89da2topic@gmail.com>
2023-08-29 11:25:07 +0900

    get thumbnailsURL

--- get.py
+++ get.py
@@ -9,20 +9,23 @@ youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
 
 search_response = youtube.search().list(
 part='snippet',
-q='桃太郎オフィス',#検索クエリ
+q='あもとっと',#検索クエリ
 order='viewCount',#視聴回数の多い順
 type='video',
 maxResults=50,
 ).execute()
 
-#print(json.dumps(search_response['items'][0]['id']['videoId'],indent=2,ensure_ascii=False))
-#videoID=search_response['items'][0]['id']['videoId']
+thumbnails=search_response['items'][0]['snippet']['thumbnails']['high']['url'] #hqのサムネurl
+maxThumbnails=thumbnails.replace('hq','maxres') #maxresに変換
+print(maxThumbnails)
+videoID=search_response['items'][0]['id']['videoId'] #print(search_response['items'][0]['id']['videoId'])
 statistics=youtube.videos().list(
     #統計情報
     part='statistics',
-    id='BUQU5BndtpQ',
+    id=f'{videoID}',
 ).execute()['items'][0]['statistics']
-#print(json.dumps(statistics['items'][0]['statistics'], indent=2, ensure_ascii=False))
+#視聴回数=viewCount 高評価数=likeCount お気に入り数=favoriteCount コメント数=commentCount
+print(statistics['viewCount']) 
 #video_list=[]
 #for item in search_response.get('items', []):
 #    if item['id']['kind'] != 'youtube#video':
