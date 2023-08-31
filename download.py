2cef5b9a25013f1b740d848b48c1a2992b4d07b5
FussyReich <okano89da2topic@gmail.com>
2023-08-31 15:20:26 +0900

    function get_...

--- download.py
+++ download.py
@@ -1,4 +1,19 @@
-import get
+import search as s
 import requests
+import time
 
-URL=get.get_thumbanailsURL
+def download():
+  nowTime=time.time()
+  i=1
+  while True:
+    if time.time() - nowTime > 5:
+      break  
+    URL=s.get_thumbanailsURL()
+    print(f'{URL}')
+    response=requests.get(URL)
+    #画像をファイルに保存
+    with open(f'./Thumbnails/{i}.png', 'wb') as f:
+      f.write(response.content)
+    i+=1
+
+download()
--- search.py
+++ search.py
@@ -4,8 +4,8 @@ import pandas as pd
 import requests
 
 YOUTUBE_API_KEY = 'AIzaSyB_VBZGfozRL1ISMUQU95WIpfOuWI7wXto'
-
-youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
+YOUTUBE_API_KEY='AIzaSyCKCZjShvqfwLk21MYu_u8MlHyMTG3m5-Q'
+youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)  
 
 #検索情報
 search_response = youtube.search().list(
@@ -15,23 +15,35 @@ order='viewCount',#視聴回数の多い順
 type='video',
 maxResults=50,
 ).execute()
-
 videoID=search_response['items'][0]['id']['videoId'] #print(search_response['items'][0]['id']['videoId'])
-statistics=youtube.videos().list(
-    part='statistics',#統計情報
-    id=f'{videoID}',
-).execute()['items'][0]['statistics']
-#視聴回数=viewCount 高評価数=likeCount お気に入り数=favoriteCount コメント数=commentCount
-print(statistics['viewCount']) 
+
 def get_thumbanailsURL():
+  i=1
   for item in search_response.get('items', []):
     if item['id']['kind'] != 'youtube#video':
       continue
     thumbnailsURL=item['snippet']['thumbnails']['high']['url']
     maxThumbnailsURL=thumbnailsURL.replace('hq', 'maxres')
-    print(json.dumps(thumbnailsURL, indent=2, ensure_ascii=False))
+    URL={
+      "maxThumbnailsURL":{
+        "url":maxThumbnailsURL,
+        "videoId":videoID,
+      }
+    }
+    with open(f'./json/{i}.json', 'w') as f:
+      json.dump(URL, f, ensure_ascii=False)
+  print(maxThumbnailsURL)
   return maxThumbnailsURL
-#get_thumbanailsURL()
 
+get_thumbanailsURL()
+
+def get_statistics():
+  statistics=youtube.videos().list(
+    part='statistics',#統計情報
+    id=f'{videoID}',
+  ).execute()['items'][0]['statistics']
+  return statistics
+  #視聴回数=viewCount 高評価数=likeCount お気に入り数=favoriteCount コメント数=commentCount
+  
 
 
