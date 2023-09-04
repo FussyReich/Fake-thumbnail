a93d6077249e80cf098babc27662fc5409b72216
FussyReich <okano89da2topic@gmail.com>
2023-09-04 20:50:38 +0900

    get channelId

--- search.py
+++ search.py
@@ -11,14 +11,30 @@ youtube = build('youtube', 'v3', developerKey=YOUTUBE_API_KEY)
 #検索情報
 search_response = youtube.search().list(
 part='snippet',
-q='あもとっと',#検索クエリ
+q='べあてぃぼん',#検索クエリ
 order='viewCount',#視聴回数の多い順
 type='video',
 maxResults=50,
 ).execute()
-#videoID=search_response['items'][0]['id']['videoId'] #print(search_response['items'][0]['id']['videoId'])
 
-def get_thumbanailsURL():
+channelID=search_response['items'][0]['snippet']['channelId']
+channel=youtube.channels().list(
+  part='snippet,contentDetails',
+  id=channelID, #channelID
+).execute()['items'][0]['contentDetails']
+playlist_id=channel['relatedPlaylists']['uploads']
+
+items_info = youtube.playlistItems().list(
+  part='contentDetails', 
+  playlistId=playlist_id, 
+  maxResults=50,
+).execute()
+video_ids = list(map(lambda item: item['contentDetails']['videoId'], items_info['items']))
+#print(json.dumps(items_info,indent=2, ensure_ascii=False))
+print(json.dumps(video_ids,indent=2, ensure_ascii=False))
+
+#print(json.dumps(channel,indent=2, ensure_ascii=False))
+def thumbanails():
   i=1
   for item in search_response.get('items', []):
     if item['id']['kind'] != 'youtube#video':
@@ -38,13 +54,19 @@ def get_thumbanailsURL():
       }
     }
     fileName=(f'./json/{i}.json')
+    #response=requests.get(maxThumbnailsURL)
+    response=requests.get(thumbnailsURL)
+    #with open(f'./Good_Thumbnails/{i}.png', 'wb') as f:
+    #  f.write(response.content)
+    with open(f'./Thumbnails/{i}.png', 'wb') as f:
+      f.write(response.content)
     with open(fileName, 'w') as f:
       json.dump(URL, f, indent=2, ensure_ascii=False)
       i+=1
-  print(maxThumbnailsURL)
+    print(maxThumbnailsURL)
   return maxThumbnailsURL
 
-get_thumbanailsURL()
+#thumbanails()
 
 
   
